1730438db21040ef14cc4fc36bf85567c6d6573b
919610459 <S545508@NWMISSOURI.EDU>
2021-12-01 00:30:43 -0600

    added data

--- models/dog.js
+++ models/dog.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
     dog_breed: String,
-    color: String,
-    maxHeight: Number
+    color: {type:String,length:9},
+    maxHeight: {type:Number,min:1,max:10}
 })
 module.exports = mongoose.model("Dog",
     dogSchema)
--- routes/dog.js
+++ routes/dog.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
+//const secured = (req, res, next) => {
+  //if (req.user){
+    //return next();
+  //}
+  //req.session.returnTo = req.originalUrl;
+  //res.redirect("/login");
+//}
 /* GET dog */
 router.get('/', dog_controlers.dog_view_all_Page );
 /* GET detail dog page */
@@ -8,7 +15,9 @@ router.get('/detail', dog_controlers.dog_view_one_Page);
 /* GET create dog page */
 router.get('/create', dog_controlers.dog_create_Page);
 /* GET create update page */
-router.get('/update', dog_controlers.dog_update_Page);
+ router.get('/update', dog_controlers.dog_update_Page);
+/* GET update dog page */
+router.get('/update',  dog_controlers.dog_update_Page);
 /* GET create dog page */
 router.get('/delete', dog_controlers.dog_delete_Page);
 
